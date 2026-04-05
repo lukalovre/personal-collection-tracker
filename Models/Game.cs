91bd52506a16162501673716e19526010fcaba75
Luka Lovre <lukalovre@protonmail.com>
2026-04-05 12:16:00 +0200

    Added developer to games collection

--- Models/Game.cs
+++ Models/Game.cs
@@ -7,12 +7,13 @@ public class Game : IItem, ICollection
 {
     [Key]
     public int ID { get; set; }
-    public string Title { get; set; }
-    public string Edition { get; set; }
+    public string Title { get; set; } = string.Empty;
+    public string Developer { get; set; } = string.Empty;
+    public string Edition { get; set; } = string.Empty;
     public bool Expansion { get; set; }
     public int Year { get; set; }
-    public string Platform { get; set; }
-    public string Client { get; set; }
+    public string Platform { get; set; } = string.Empty;
+    public string Client { get; set; } = string.Empty;
     public bool New { get; set; }
     public bool GotFree { get; set; }
     public DateTime? Date { get; set; }
@@ -20,7 +21,7 @@ public class Game : IItem, ICollection
     public float? PriceInRSD { get; set; }
     public bool PhysicalCopy { get; set; }
     public int HLTB { get; set; }
-    public string Owner { get; set; }
-    public string ExternalID { get; set; }
+    public string Owner { get; set; } = string.Empty;
+    public string ExternalID { get; set; } = string.Empty;
     public bool? Bookmarked { get; set; }
 }
